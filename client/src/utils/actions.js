c0103aa6e331c3ddbfaeda5dab0c83ff29ddfc5e
XXLZopes <afordm99@gmail.com>
2021-03-25 13:42:51 -0700

    added cart functionality

--- client/src/utils/actions.js
+++ client/src/utils/actions.js
@@ -13,3 +13,12 @@ export const UPDATE_CATEGORIES = "UPDATE_CATEGORIES";
 //We need to be able to select a category from the state created by UPDATE_CATEGORIES and display products for that category from the UPDATE_PRODUCTS
 export const UPDATE_CURRENT_CATEGORY = "UPDATE_CURRENT_CATEGORY";
 
+
+//cart actions
+export const ADD_TO_CART = 'ADD_TO_CART';
+export const ADD_MULTIPLE_TO_CART = 'ADD_MULTIPLE_TO_CART';
+export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
+export const UPDATE_CART_QUANTITY = 'UPDATE_CART_QUANTITY';
+export const CLEAR_CART = 'CLEAR_CART';
+export const TOGGLE_CART = 'TOGGLE_CART';
+
