a2e0b2817ce95156a2be96ef75c7c738f979c7b4
XXLZopes <afordm99@gmail.com>
2021-04-08 13:45:48 -0700

    done

--- client/src/utils/actions.js
+++ client/src/utils/actions.js
@@ -1,24 +1,11 @@
-//These actions define how our state will be maintained and updated
-
-//used by the ProductList component.
-//Rn product data is from server and Apollo caches the results
-//Goal is to store data retrieved for products in this global state to enable offline functionality later on
 export const UPDATE_PRODUCTS = "UPDATE_PRODUCTS";
 
-//Takes list of categories retrieved from the server by Apollo and store it in global state.
-//Help easily add offline capabilities.
+export const ADD_TO_CART = "ADD_TO_CART";
+export const ADD_MULTIPLE_TO_CART = "ADD_MULTIPLE_TO_CART";
+export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
+export const CLEAR_CART = "CLEAR_CART";
+export const UPDATE_CART_QUANTITY = "UPDATE_CART_QUANTITY";
+export const TOGGLE_CART = "TOGGLE_CART";
+
 export const UPDATE_CATEGORIES = "UPDATE_CATEGORIES";
-
-//Connecting piece of data for UPDATE_PRODUCTS & UPDATE_CATEGORIES
-//We need to be able to select a category from the state created by UPDATE_CATEGORIES and display products for that category from the UPDATE_PRODUCTS
 export const UPDATE_CURRENT_CATEGORY = "UPDATE_CURRENT_CATEGORY";
-
-
-//cart actions
-export const ADD_TO_CART = 'ADD_TO_CART';
-export const ADD_MULTIPLE_TO_CART = 'ADD_MULTIPLE_TO_CART';
-export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
-export const UPDATE_CART_QUANTITY = 'UPDATE_CART_QUANTITY';
-export const CLEAR_CART = 'CLEAR_CART';
-export const TOGGLE_CART = 'TOGGLE_CART';
-
